b51f10b609f86b9c3b7a5906d812d25ed2cae85e
Thomas Nguyen <144487147+ThomasNguyen1114@users.noreply.github.com>
2025-04-04 23:11:45 -0500

    Update scientistController.php

--- MVC-Lab/controllers/scientistController.php
+++ MVC-Lab/controllers/scientistController.php
@@ -1,63 +1,64 @@
-<?php 
-    class ScientistList implements controllerAction {
+<?php
+require_once 'model/Scientist.php';
+require_once 'model/ScientistDAO.php';
 
-        public function processGET() {
+class ScientistList implements controllerAction {
+
+    public function processGET() {
+        $scientistDAO = new ScientistDAO();
+        $scientists = $scientistDAO->getScientists(); 
+        include 'views/listScientists.php'; 
+    }
+
+    public function processPOST() {
+        return;
+    }
+}
+
+class ScientistAdd implements controllerAction {
+
+    public function processGET() {
+        include 'views/addScientist.php'; 
+    }
+
+    public function processPOST() {
+        $name = $_POST['name'] ?? '';
+        $timePeriod = $_POST['timePeriod'] ?? '';
+        $nationality = $_POST['nationality'] ?? '';
+        $contribution = $_POST['contribution'] ?? '';
+
+        $scientist = new Scientist();
+        $scientist->setName($name);
+        $scientist->setTimePeriod($timePeriod);
+        $scientist->setNationality($nationality);
+        $scientist->setContribution($contribution);
+
+        $scientistDAO = new ScientistDAO();
+        $scientistDAO->addScientist($scientist);
+
+        header("Location: scienceController.php?page=list");
+        exit;
+    }
+}
+
+class ScientistDelete implements controllerAction {
+
+    public function processGET() {
+        $id = $_GET['id'] ?? null;
+        include 'views/deleteScientist.php'; 
+    }
+
+    public function processPOST() {
+        $id = $_POST['id'] ?? null;
+        $submit = $_POST['submit'] ?? '';
+
+        if ($submit === 'CONFIRM' && $id) {
             $scientistDAO = new ScientistDAO();
-            $scientists = $scientistDAO->getScientists();
-            include 'views/listScientists.php';
+            $scientistDAO->deleteScientist($id);
         }
 
-        public function processPOST() {
-            return;
-        }
-
+        header("Location: scienceController.php?page=list");
+        exit;
     }
-
-    class ScientistAdd implements controllerAction {
-
-        public function processGET() {
-            include 'views/addScientist.php';
-        }
-
-        public function processPOST() {
-            $name = $_POST['name'];
-            $timePeriod = $_POST['timePeriod'];
-            $nationality = $_POST['nationality'];
-            $contribution = $_POST['contribution'];
-            
-            $scientist = new Scientist();
-            $scientist->setName($name);
-            $scientist->setTimePeriod($timePeriod);
-            $scientist->setNationality($nationality);
-            $scientist->setContribution($contribution);
-
-            $scientistDAO = new ScientistDAO();
-            $scientistDAO->addScientist($scientist);
-
-            header("Location: scienceController.php?page=list");
-            exit;
-        }
-
-    }
-
-    class ScientistDelete implements controllerAction {
-
-        public function processGET() {
-            $id = $_GET['id'];
-            include 'views/deleteScientist.php';
-        }
-
-        public function processPOST() {
-            $id = $_POST['id'];
-            $submit = $_POST['submit'];
-
-            if ($submit == 'CONFIRM') {
-                $scientistDAO = new ScientistDAO();
-                $scientistDAO->deleteScientist($id);
-            }
-
-            header("Location: scienceController.php?page=list");
-            exit;
-        }
-    }
+}
 ?>
